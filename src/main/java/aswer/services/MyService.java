04d0f3eaac392a9c8d3921c50fce056ae2669fc5
root <root@ip-172-31-90-164.ec2.internal>
2022-12-17 14:25:41 +0000

    read all files

---
+++ src/main/java/aswer/services/MyService.java
@@ -0,0 +1,13 @@
+package aswer.services;
+
+import aswer.entity.Log;
+import java.util.List;
+
+public interface MyService {
+
+	Log addLog(Log log);
+
+	List<Log> getLogs();
+
+	void deleteLog(Log log);
+}
